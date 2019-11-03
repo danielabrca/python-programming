4d26c1c7a25284ec884cc0ac5b5ceab41620fb6e
= <=>
2019-11-03 12:03:27 -0500

    Unit 2 Homework - Phase 2 - Problem 2

--- unit-2/homework/hw-6.py
+++ unit-2/homework/hw-6.py
@@ -1,2 +1,15 @@
-marvel_movies = ['Captain America: The First Avenger', 'Iron Man', 'Guardians of the Galaxy', 'The Incredible Hulk', 'The Avengers', 'Ant-Man', 'Spider-Man: Homecoming', 'Doctor Strange', 'Black Panther', 'Avengers: Infinity War', 'Captain Marvel']
-print(marvel_movies)
+#list of the Marvel movies
+marvel_movies = ['Captain America: The First Avenger', 'Iron Man', 'Guardians of the Galaxy', 'The Incredible Hulk',
+                 'The Avengers', 'Ant-Man', 'Spider-Man: Homecoming', 'Doctor Strange', 'Black Panther',
+                 'Avengers: Infinity War', 'Captain Marvel', 'Thor: The Dark World', 'The Amazing Spider-Man']
+
+#Marvel movies that contain word "The"
+special_marvel_movies = []
+
+#iterating each movie in marvel_movies
+for idx in marvel_movies:
+    #checking condition
+    if 'The' in idx:
+        special_marvel_movies.append(idx)
+
+print(special_marvel_movies)
