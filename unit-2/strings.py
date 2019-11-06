a288c4276816080c51c56596990e0ce7a9c07393
= <=>
2019-11-06 18:49:04 -0500

    Strings

---
+++ unit-2/strings.py
@@ -0,0 +1,13 @@
+#strings are immutable
+
+movie_title = 'Thor, the Dark World'
+
+print(movie_title)
+
+movie_title = 'The Avengers' #assigned a new movie to the variable
+
+start = movie_title[0:2] #if there is just one item in your list os strings, it would work inside the item looking for each character
+
+print(start)
+print(movie_title[-1]) #last character
+print(movie_title[:4])
